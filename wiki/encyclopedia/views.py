50bc9f1b4b287ad875742ff2dd98665dcdb60149
Chandra Shekhar <shekhar396tewm@gmail.com>
2024-06-28 17:08:13 +0530

    Entry page added and Index page is updated

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from . import util
+from markdown2 import markdown
 
 
 def index(request):
@@ -8,3 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry_page(request, title):
+    if util.get_entry(title) is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
+    else:
+        return render(request, "encyclopedia/entrypage.html",{
+            "content": markdown(util.get_entry(title)),
+            "title": title
+        })
